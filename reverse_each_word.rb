8d317862ced5699e96d7b44e07b5a5271bc7c084
apocalypse575 <apocalypse575@gmail.com>
2017-12-14 15:36:33 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(string)
-  
+
 end
+
+
+#reverse_each_word reverses all the words in a string without reversing the order of the words
+
+#reverse_each_word reverses all the words in another string without reversing the order of the words
