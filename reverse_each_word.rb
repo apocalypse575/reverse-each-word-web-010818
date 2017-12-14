7359bc855e0a196fe176a06fc477bf33d95be9ba
apocalypse575 <apocalypse575@gmail.com>
2017-12-14 16:07:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,5 @@
-# def reverse_each_word(words)
-#   words.reverse.split.reverse.join(" ") do |reverse|
-#     puts words.collect
-#   end
-# end
-
 def reverse_each_word(words)
   words.split.collect do |reverse|
     return words.reverse.split.reverse.join(" ")
   end
 end
-
-
-#reverse_each_word reverses all the words in a string without reversing the order of the words
-
-#reverse_each_word reverses all the words in another string without reversing the order of the words
